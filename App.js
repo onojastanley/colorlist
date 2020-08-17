9d7b58c88265356eb81d93ebe02cace776b931aa
onojastanley <40136094+onojastanley@users.noreply.github.com>
2020-08-17 18:34:58 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
     backgroundColor:'yellow',
 
-    };
+    }
+    this.changeColor = this.changeColor.bind(this)
+  }
+
+  changeColor(backgroundColor) {
+    this.setState({backgroundColor})
   }
 
   render() {
     const backgroundColor = this.state.backgroundColor;
     return (
       <View style={[styles.container,{backgroundColor} ]}>
-        <Text>Hello, worldyy!</Text>
+        <Text style={styles.button}
+          onPress={() => this.changeColor('yellow')}>yellow</Text>
+              <Text style={styles.button}
+          onPress={() => this.changeColor('green')}>green</Text>
+        <Text style={styles.button}
+          onPress={() => this.changeColor('red')}>red</Text>
         
       </View>
     );
@@ -26,6 +36,15 @@ const styles = StyleSheet.create(
       backgroundColor: 'blue',
       alignItems: 'center',
       justifyContent: 'center'
+    },
+    button: {
+      fontSize: 30,
+      margin: 10,
+      padding: 10,
+      borderWidth: 2,
+      borderRadius: 10,
+      alignSelf: 'stretch',
+      textAlign: 'center'
     }
   }
 
